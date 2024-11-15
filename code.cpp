7514ea5feda42f6c4a318421004f9523dae711c8
unknown <thanhphuongtransd@gmail.com>
2024-11-16 02:42:39 +0700

    a

--- code.cpp
+++ code.cpp
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
-
 public class PhepChia {
+    // Hàm tính UCLN bằng thuật toán Euclid
+    public static int UCLN(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -20,43 +29,34 @@ public class PhepChia {
             System.out.println("Thương của " + a + " và " + b + " là: " + thuong);
         }
 
+        // Hàm tính UCLN
+        System.out.print("Nhập số nguyên thứ nhất (UCLN): ");
+        int num1 = scanner.nextInt();
+        System.out.print("Nhập số nguyên thứ hai (UCLN): ");
+        int num2 = scanner.nextInt();
+
+        // Tính và in ra UCLN
+        System.out.println("Ước chung lớn nhất của " + num1 + " và " + num2 + " là: " + UCLN(num1, num2));
+
+        // Kiểm tra số chẵn hay lẻ
+        System.out.print("Nhập một số nguyên: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 != 0) {
+            System.out.println(number + " là số lẻ.");
+        } else {
+            System.out.println(number + " là số chẵn.");
+        }
+
+        // Thêm phần tính tổng
+        System.out.print("Nhập một số nguyên thứ nhất để tính tổng: ");
+        int num3 = scanner.nextInt();
+        System.out.print("Nhập một số nguyên thứ hai để tính tổng: ");
+        int num4 = scanner.nextInt();
+
+        int sum = num3 + num4;
+        System.out.println("Tổng của " + num3 + " và " + num4 + " là: " + sum);
+
         scanner.close();
     }
 }
-
-// Hàm tính UCLN bằng thuật toán Euclid
-
-int main() {
-
-    int num1, num2;
-
-    // Nhập hai số nguyên từ người dùng
-    cout << "Nhập số thứ nhất: ";
-    cin >> num1;
-    cout << "Nhập số thứ hai: ";
-    cin >> num2;
-
-    // Tính và in ra UCLN
-    cout << "Ước chung lớn nhất của " << num1 << " và " << num2 << " là: " << UCLN(num1, num2) << endl;
-
-    int number;
-	int n =0 ;
-    // Nhập số từ người dùng
-    cout << "Nhập một số nguyên: ";
-    cin >> number;
-	
-    // Kiểm tra nếu số chia hết cho 2 (số chẵn) hoặc không chia hết (số lẻ)
-    if (number % 2 != 0) {
-        cout << number << " là số lẻ." << endl;
-    } else {
-        cout << number << " là số chẵn." << endl;
-    }
-
-
-    return 0;
-}
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 10c7ffa93b34fa3576d264ba7a6ad68de0e3d1f8
